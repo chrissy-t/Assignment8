cd209569030b830ee292add3c69b1bdb0ff8c49a
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2021-11-02 11:55:41 -0400

    added stuff

---
+++ script.js
@@ -0,0 +1,9 @@
+var vaccinated = true;
+var name = 'Dude';
+var message = 'Hello'+ name + 'Vaccination Card Approved, Welcome to Dp Cakery';
+
+var element = document.getElementByID('boba');
+
+element.textContext = message;
+
+console.log(message);
