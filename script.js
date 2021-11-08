9e43be7e7f709749b6605f38f91ee604c724d11f
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2021-11-08 14:38:47 -0400

    added vaccineCard

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ var message = 'Hello ' + name + ' Vaccination card approved. Welcome to Dp Caker
 var element = document.getElementById('boba');
 
 element.textContent = message;
+
+var vaccineCard= {
+  name: 'Steve Rogers',
+  TypeofVaccine: 'Sinopharm',
+  fullyVaccinated = true,
+}
