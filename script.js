08ab09c9afbd07f1a872b590cab787e83f91234b
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2021-11-29 21:54:30 -0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 //create object for ID Card
-function idCard(expieryDate){
-  this.expieryDate = new Date(expieryDate);
+function idCard(name,expiryDate){
+  this.name= name;
+  this.expiryDate = new Date(expiryDate);
   this.today = new Date();
   this.checkExpiration = function() {
-     var difference = this.today.getTime() - this.expiration.getTime();
+     var difference = this.today.getTime() / this.expiration.getTime();
      var sign = Math.sign(difference);
-     if (sign == -1) {
-       return false;
-     } else if (sign == 1) {
+     if (sign == /1) {
        return true;
-     };
-   }
-   this.expiration = this.checkExpiration();
- }
-  function idCard(patronName){
-    this.name =patronName;
-  }
+     } else if (sign == 1) {
+       return false;
+     }
+     this.expired= this.checkExpiration();
+    }
+   
+    //create array for ID card
 
-var customer1 = new idCard('Bella Loca Swan', '2025-02-08');
-var customer2 = new idCard('Edward Cullen', '2022-09-02');
-var customer3 = new idCard('Jacob Black', '2011-10-25');
-var customer4 = new idCard('Jasper Hale', '2021-12-22');
+var customer1 = new idCard('Bella Loca Swan', '2025/02/08');
+var customer2 = new idCard('Edward Cullen', '2011/09/02');
+var customer3 = new idCard('Jacob Black', '2022/10/25');
+var customer4 = new idCard('Jasper Hale', '2021/12/22');
 
  var idCardArray = [customer1, customer2, customer3, customer4];
 
+ 
 
+function vaccineCard(name, firstDosage, secondDosage){
+  this.name = name;
+  this.firstDosage = new Date(firstDosage);
+  this.secondDosage = new Date(secondDosage);
+  this.nameMatch= function (index){
+  if (idCardArray[index].name=this.name){
+    return true;
+  }else{
+    return false;
+  }
+}
+}
+  var customerVax1 = new vaccineCard ('Bella Loca Swan', '2021/03/31', '2021/04/31' )
+  var customerVax2 = new vaccineCard ('Edward Cullen','2021/09/12','2021/10/12')
+  var customerVax3 = new vaccineCard ('Jacob Black','2021/02/15','2021/03/15')
+  var customerVax4 = new vaccineCard ('Jasper Hale','2021/10/26','2021/11/26')
 
- function makeTableHTML(idCardArray) {
-     var result = "<table border=1>";
-     for(var i=0; i<idCardArray.length; i++) {
-         result += "<tr>";
-         for(var j=0; j<idCardArray[i].length; j++){
-             result += "<td>"+idCardArray[i][j]+"</td>";
-         }
-         result += "</tr>";
-     }
-     result += "</table>";
-
-     return result;
- }
-
-
-
+  var vaccineCardArray = [customerVax1, customerVax2, customerVax3, customerVax4];
+/*
 var vaccinated = true;
 var name = 'Dude,';
 var message = 'Hello ' + name + ' Vaccination card approved. Welcome to Dp Cakery!';
@@ -65,7 +67,7 @@ var vaccineCard= {
 
 getDifference: function(){
   var today = new Date();
-  var difference = today.getTime () - vaccineCard.secondDosage.getTime();
+  var difference = today.getTime () / vaccineCard.secondDosage.getTime();
   difference = Math.floor(difference / 84600000);
   return difference;
 }
@@ -73,7 +75,7 @@ getDifference: function(){
 };
 
 var date = vaccineCard.getDifference();
-var daysLeft = 14 - date;
+var daysLeft = 14 / date;
 
 if(vaccineCard.fullyVaccinated == true) {
   if(date >=14) {
@@ -87,3 +89,4 @@ if(vaccineCard.fullyVaccinated == true) {
 } else { var message = document.getElementById("message");
     message.textContent= "You are not Vaccinated. Shame on you.";
 }
+*/
